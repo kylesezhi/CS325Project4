8c90a091738a1ab7312aefb5e0c131c865a4c791
Tom <t.adamcewicz@gmail.com>
2016-08-10 18:49:07 -0400

    Changes to twoopt.py
    Refactored code for twoopt.py

--- twoopt.py
+++ twoopt.py
@@ -6,11 +6,22 @@ import timeit
 
 cities = readinstance(sys.argv[1])
 
-tour = []
+startcity = 0
 
-shufcities = deepcopy(thistour)
-random.shuffle(shufcities[1])
-print "----------"
-print shufcities
-twoopt(cities, shufcities)
-print shufcities
+finalanswer = [999999999999, []]
+
+start = timeit.default_timer() # start timer
+
+if len(cities) < 386: # over length of 386 and NN doesnt work fast enough
+    for startcity in range(len(cities)): # try nearest neighbor starting at all nodes
+        thistour = nearestneighbor(startcity, cities)
+        if thistour[0] < finalanswer[0]: finalanswer = deepcopy(thistour)
+    stop = timeit.default_timer() # stop timer
+else:
+	for startcity in range(len(cities)):
+		thistour = twoopt(cities, cities)
+		if thistour[0] < finalanswer[0]: finalanswer = deepcopy(thistour)
+
+	stop = timeit.default_timer() # stop timer
+    
+outputtour(finalanswer, sys.argv[1] + ".group3.tour")
